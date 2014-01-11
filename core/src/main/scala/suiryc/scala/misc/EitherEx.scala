8b886bf7b0039e7933ad33426c956e9ddf8e90e1
Julien Coloos <julien.coloos@gmail.com>
2014-01-11 23:09:08 +0100

    fixup

--- core/src/main/scala/suiryc/scala/misc/EitherEx.scala
+++ core/src/main/scala/suiryc/scala/misc/EitherEx.scala
@@ -1,9 +1,9 @@
 package suiryc.scala.misc
 
 
-abstract class EitherEx[+A, +B](val underlying: Either[A, B]) {
+abstract class EitherEx[+A, +B](val either: Either[A, B]) {
 
-  def apply: B
+  def apply(): B
 
 }
 
@@ -12,17 +12,17 @@ object EitherEx {
 
   def apply[A, B](right: Right[A, B]) =
     new EitherEx(right) {
-      override def apply = right.b
+      override def apply() = right.b
     }
 
   def apply[A, B](left: Left[A, B], default : => B) =
     new EitherEx(left) {
-      override def apply = default
+      override def apply() = default
     }
 
   def apply[A, B](either: Either[A, B], default : => B) =
     new EitherEx(either) {
-      override def apply = underlying match {
+      override def apply() = either match {
         case Left(_) =>
           default
 
