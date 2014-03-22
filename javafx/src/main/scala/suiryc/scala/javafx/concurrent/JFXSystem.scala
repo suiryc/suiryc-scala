065f32fb071aa5bc77c238467f7b357fdaaf609f
Julien Coloos <julien.coloos@gmail.com>
2014-03-22 16:19:36 +0100

    Some more schedule methods for JavaFX system

--- javafx/src/main/scala/suiryc/scala/javafx/concurrent/JFXSystem.scala
+++ javafx/src/main/scala/suiryc/scala/javafx/concurrent/JFXSystem.scala
@@ -2,6 +2,7 @@ package suiryc.scala.javafx.concurrent
 
 import akka.actor.{Actor, ActorSystem, Props}
 import com.typesafe.config.ConfigFactory
+import scala.concurrent.duration.FiniteDuration
 
 
 object JFXSystem {
@@ -12,8 +13,16 @@ object JFXSystem {
   val system = ActorSystem("javafx", config)
   val jfxActor = system.actorOf(Props[JFXActor].withDispatcher("suiryc.javafx.dispatcher"), "JavaFX-dispatcher")
 
+  import system.dispatcher
+
   def schedule(action: => Unit) =
     jfxActor ! { () => action }
+
+  def schedule(initialDelay: FiniteDuration, interval: FiniteDuration)(action: => Unit) =
+    system.scheduler.schedule(initialDelay, interval, jfxActor, { () => action })
+
+  def scheduleOnce(delay: FiniteDuration)(action: => Unit) =
+    system.scheduler.scheduleOnce(delay, jfxActor, { () => action })
 
   class JFXActor extends Actor {
 
