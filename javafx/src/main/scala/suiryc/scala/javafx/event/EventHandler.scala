d2736adbca4b93f47ee9ebc94974623396e43f6c
Julien Coloos <julien.coloos@gmail.com>
2014-07-05 22:01:18 +0200

    Added Function1 to JavaFX EventHandler conversion function

---
+++ javafx/src/main/scala/suiryc/scala/javafx/event/EventHandler.scala
@@ -0,0 +1,16 @@
+package suiryc.scala.javafx.event
+
+import javafx.{event => jfxe}
+
+object EventHandler {
+
+  import scala.language.implicitConversions
+
+  implicit def fn1ToEventHandler[T <: jfxe.Event](fn: Function1[T, Unit]): jfxe.EventHandler[T] = {
+    new jfxe.EventHandler[T] {
+      override def handle(event: T) =
+        fn(event)
+    }
+  }
+
+}
