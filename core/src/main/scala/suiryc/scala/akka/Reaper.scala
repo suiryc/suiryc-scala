94df697df947e3a6d743d291184efae269187e1a
Julien Coloos <julien.coloos@gmail.com>
2014-11-01 22:40:36 +0100

    Add akka system actors Reaper

---
+++ core/src/main/scala/suiryc/scala/akka/Reaper.scala
@@ -0,0 +1,64 @@
+package suiryc.scala.akka
+
+import akka.actor.{Actor, ActorRef, Terminated}
+import grizzled.slf4j.Logging
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Reaper companion object.
+ */
+object Reaper {
+
+  /** Actor message: register an Actor for watching. */
+  case class WatchMe(ref: ActorRef)
+
+}
+
+/**
+ * Actors system reaper.
+ *
+ * The reaper watch over a list of registered actors, and call `allSoulsReaped`
+ * once all actors terminated.
+ *
+ * @see [[http://letitcrash.com/post/30165507578/shutdown-patterns-in-akka-2]]
+ */
+abstract class Reaper
+  extends Actor
+  with Logging
+{
+  import Reaper._
+
+  /** Watched actors. */
+  protected val watched = ArrayBuffer.empty[ActorRef]
+
+  /**
+   * Subclasses need to implement this method. It's the hook that's called when
+   * everything's dead.
+   */
+  protected def allSoulsReaped(): Unit
+
+  /** Watch and check for termination. */
+  final override def receive = {
+    case WatchMe(ref) =>
+      trace(s"Watching $ref")
+      context.watch(ref)
+      watched += ref
+
+    case Terminated(ref) =>
+      trace(s"$ref terminated")
+      watched -= ref
+      if (watched.isEmpty) {
+        debug("All souls reaped")
+        allSoulsReaped()
+      }
+  }
+
+}
+
+/** Simple reaper that shutdowns the system once finished. */
+class ShutdownReaper extends Reaper {
+
+  /** Shutdown */
+  override protected def allSoulsReaped(): Unit = context.system.shutdown()
+
+}
