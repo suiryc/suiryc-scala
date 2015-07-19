9b300f0b5a1b82ebbd4968f613744f4fe1fdc5cb
Julien Coloos <julien.coloos@gmail.com>
2015-07-19 22:47:28 +0200

    Removed useless intermediary trait in setting snapshot code.
    Added scaladoc.

--- core/src/main/scala/suiryc/scala/settings/SettingSnapshot.scala
+++ core/src/main/scala/suiryc/scala/settings/SettingSnapshot.scala
@@ -2,28 +2,29 @@ package suiryc.scala.settings
 
 import javafx.beans.property.Property
 
+/**
+ * Setting snapshot.
+ *
+ * Allows to determine if a setting was changed and reset it.
+ */
+trait SettingSnapshot[T] {
 
-trait SettingSnapshot {
-
-  def changed(): Boolean
-
-  def reset(): Unit
-
-}
-
-trait SettingValueSnapshot[T] extends SettingSnapshot {
-
+  /** Original value. */
   protected val originalValue =
     getValue
 
+  /** Gets current value. */
   protected def getValue: T
 
+  /** Sets value. */
   protected def setValue(v: T): Unit
 
-  override def changed() =
+  /** Gets whether the setting was changed. */
+  def changed(): Boolean =
     originalValue != getValue
 
-  override def reset() {
+  /** Resets the setting to its initial value. */
+  def reset(): Unit = {
     if (changed()) setValue(originalValue)
   }
 
@@ -31,8 +32,9 @@ trait SettingValueSnapshot[T] extends SettingSnapshot {
 
 object SettingSnapshot {
 
-  def apply[T](setting: PersistentSetting[T]): SettingSnapshot =
-    new SettingValueSnapshot[T] {
+  /** Builds a snapshot from a persistent setting. */
+  def apply[T](setting: PersistentSetting[T]): SettingSnapshot[T] =
+    new SettingSnapshot[T] {
 
       override protected def getValue =
         setting()
@@ -43,8 +45,9 @@ object SettingSnapshot {
 
     }
 
-  def apply[T](property: Property[T]): SettingSnapshot =
-    new SettingValueSnapshot[T] {
+  /** Builds a snapshot from a property. */
+  def apply[T](property: Property[T]): SettingSnapshot[T] =
+    new SettingSnapshot[T] {
 
       override protected def getValue =
         property.getValue
@@ -57,18 +60,27 @@ object SettingSnapshot {
 
 }
 
-
+/**
+ * Settings snapshot.
+ *
+ * Hold a list of snapshots. Allows to determine if any changed, and reset
+ * them all.
+ */
 class SettingsSnapshot {
 
-  private var snapshots: List[SettingSnapshot] = Nil
+  /** Setting snapshots. */
+  private var snapshots: List[SettingSnapshot[_]] = Nil
 
-  def add(others: SettingSnapshot*) {
+  /** Adds a setting snapshot. */
+  def add(others: SettingSnapshot[_]*) {
     snapshots ++= others.toList
   }
 
+  /** Gets whether any setting changed. */
   def changed() =
     snapshots.exists(_.changed())
 
+  /** Resets all settings. */
   def reset() {
     snapshots.foreach(_.reset())
   }
