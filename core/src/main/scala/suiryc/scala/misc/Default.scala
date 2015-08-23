0f68e8be86e89429c73a3610443e7744d21081cb
Julien Coloos <julien.coloos@gmail.com>
2015-08-23 22:13:09 +0200

    Helper class to get default value for a given type
    Not used for now, but still a useful trick to keep

---
+++ core/src/main/scala/suiryc/scala/misc/Default.scala
@@ -0,0 +1,30 @@
+package suiryc.scala.misc
+
+trait Default[A] {
+  def value: A
+}
+
+trait LowPriorityImplicitsForDefault { this: Default.type =>
+  implicit def forAnyRef[A](implicit ev: Null <:< A) = Default.withValue(ev(null))
+}
+
+/**
+ * Helper to get default value for a given type.
+ *
+ * See: http://missingfaktor.blogspot.fr/2011/08/emulating-cs-default-keyword-in-scala.html
+ */
+object Default extends LowPriorityImplicitsForDefault {
+  def withValue[A](a: A) = new Default[A] {
+    def value = a
+  }
+
+  implicit val forBoolean = Default.withValue(false)
+  implicit val forChar = Default.withValue(' ')
+  implicit def forNumeric[A](implicit n: Numeric[A]) = Default.withValue(n.zero)
+  implicit val forString = Default.withValue("")
+  implicit def forOption[A] = Default.withValue(None:Option[A])
+  // XXX - default value for a collection would be 'empty'
+
+  /** Gets the default value for a given type. */
+  def value[A : Default] = implicitly[Default[A]].value
+}
