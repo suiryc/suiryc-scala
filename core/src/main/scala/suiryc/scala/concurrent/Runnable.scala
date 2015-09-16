99238ab2f3aedf6e84a61d6ab2aae80e0bf8bf03
Julien Coloos <julien.coloos@gmail.com>
2015-09-16 17:51:23 +0200

    Handle both implicit and explicit creation for Callback and Runnable

--- core/src/main/scala/suiryc/scala/concurrent/Runnable.scala
+++ core/src/main/scala/suiryc/scala/concurrent/Runnable.scala
@@ -2,11 +2,18 @@ package suiryc.scala.concurrent
 
 import java.lang.{Runnable => jRunnable}
 
+/** java.lang.Runnable helpers. */
 object Runnable {
 
-  /** Creates a java.lang.Runnable with a by-name parameter. */
+  import scala.language.implicitConversions
+
+  /** Creates a Runnable from a by-name parameter. */
   def apply(action: => Unit): jRunnable = new jRunnable {
     override def run() = action
   }
+
+  /** Implicit conversion from function to Runnable. */
+  implicit def fn0ToRunnable(fn: () => Unit): jRunnable =
+    Runnable(fn())
 
 }
--- javafx/src/main/scala/suiryc/scala/javafx/util/Callback.scala
+++ javafx/src/main/scala/suiryc/scala/javafx/util/Callback.scala
@@ -1,16 +1,19 @@
 package suiryc.scala.javafx.util
 
-import javafx.{util => jfxu}
+import javafx.util.{Callback => jfxCallback}
 
+/** javafx.util.Callback helpers. */
 object Callback {
 
   import scala.language.implicitConversions
 
-  implicit def fn1ToCallback[P, R](fn: P => R): jfxu.Callback[P, R] = {
-    new jfxu.Callback[P, R] {
-      override def call(p: P): R =
-        fn(p)
-    }
+  /** Creates a Callback from a function. */
+  def apply[P, R](fn: P => R): jfxCallback[P, R] = new jfxCallback[P, R] {
+    override def call(p: P): R = fn(p)
   }
+
+  /** Implicit conversion from function to Callback. */
+  implicit def fn1ToCallback[P, R](fn: P => R): jfxCallback[P, R] =
+    Callback(fn)
 
 }
