e48e50a0662eac2687720c8dc6ba9b974f330c09
Julien Coloos <julien.coloos@gmail.com>
2015-09-16 21:37:29 +0200

    Also creates Callback from Function0

--- javafx/src/main/scala/suiryc/scala/javafx/util/Callback.scala
+++ javafx/src/main/scala/suiryc/scala/javafx/util/Callback.scala
@@ -12,8 +12,17 @@ object Callback {
     override def call(p: P): R = fn(p)
   }
 
+  /** Creates a Callback from a function. */
+  def apply[P, R](fn: => R): jfxCallback[P, R] = new jfxCallback[P, R] {
+    override def call(p: P): R = fn
+  }
+
   /** Implicit conversion from function to Callback. */
   implicit def fn1ToCallback[P, R](fn: P => R): jfxCallback[P, R] =
     Callback(fn)
+
+  /** Implicit conversion from function to Callback. */
+  implicit def fn0ToCallback[P, R](fn: () => R): jfxCallback[P, R] =
+    Callback(fn())
 
 }
