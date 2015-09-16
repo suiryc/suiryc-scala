8055b1bbbe8d9389153e023442ff954ea70d5c46
Julien Coloos <julien.coloos@gmail.com>
2015-09-16 22:13:19 +0200

    Added Dialog/Alert helper class
    Function to build and show error Alert with stacktrace as content

---
+++ javafx/src/main/scala/suiryc/scala/javafx/scene/control/Dialogs.scala
@@ -0,0 +1,52 @@
+package suiryc.scala.javafx.scene.control
+
+import java.io.{PrintWriter, StringWriter}
+import javafx.scene.control.{Alert, ButtonType, Label, TextArea}
+import javafx.scene.layout.{GridPane, Priority}
+import javafx.stage.Window
+import suiryc.scala.RichOption._
+import suiryc.scala.javafx.concurrent.JFXSystem
+
+/** Dialog/Alert helpers. */
+object Dialogs {
+
+  /** Builds and shows Alert dialog with stacktrace as content. */
+  // TODO - link to official online details
+  def error(owner: Option[Window], title: Option[String], headerText: Option[String], ex: Throwable): Option[ButtonType] = {
+    // Note: it is mandatory to create the 'Alert' inside JavaFX thread.
+    JFXSystem.await({
+      val alert = new Alert(Alert.AlertType.ERROR)
+      owner.foreach(alert.initOwner)
+      title.foreach(alert.setTitle)
+      alert.setHeaderText(headerText.orNull)
+      alert.setContentText(null)
+
+      val sw = new StringWriter()
+      val pw = new PrintWriter(sw)
+      ex.printStackTrace(pw)
+      val exceptionText = sw.toString
+
+      // TODO - i18n ?
+      val label = new Label("The exception stacktrace was:")
+
+      val textArea = new TextArea(exceptionText)
+      textArea.setEditable(false)
+      textArea.setWrapText(true)
+
+      textArea.setMaxWidth(Double.MaxValue)
+      textArea.setMaxHeight(Double.MaxValue)
+      GridPane.setVgrow(textArea, Priority.ALWAYS)
+      GridPane.setHgrow(textArea, Priority.ALWAYS)
+
+      val expContent = new GridPane()
+      expContent.setMaxWidth(Double.MaxValue)
+      expContent.add(label, 0, 0)
+      expContent.add(textArea, 0, 1)
+
+      alert.getDialogPane.setExpandableContent(expContent)
+
+      alert.showAndWait()
+    }, logReentrant = false)
+  }
+
+}
