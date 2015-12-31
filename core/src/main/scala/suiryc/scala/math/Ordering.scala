50c82a04ea725022cdd3d727f87a701ed175237e
Julien Coloos <julien.coloos@gmail.com>
2015-12-31 16:01:24 +0100

    Added an Ordering helper

---
+++ core/src/main/scala/suiryc/scala/math/Ordering.scala
@@ -0,0 +1,22 @@
+package suiryc.scala.math
+
+import java.time.LocalDate
+import java.time.chrono.ChronoLocalDate
+
+/** Ordering helpers. */
+object Ordering {
+
+  /**
+    * Creates an Ordering from classes extending Comparable in a superclass.
+    *
+    * A class 'A' may subclass 'B' while 'B' is a 'Comparable[B]', in which case
+    * default Ordering implicits don't kick in for 'A'.
+    */
+  def comparableOrdering[A <: Comparable[B], B >: A]: Ordering[A] = new Ordering[A] {
+    def compare(a1: A, a2: A): Int = a1.compareTo(a2)
+  }
+
+  /** Ordering for LocalDate. */
+  implicit val localDateOrdering = comparableOrdering[LocalDate, ChronoLocalDate]
+
+}
