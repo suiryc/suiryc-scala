dc666f1064dd3f448716e21dc9d085e6042927e8
Julien Coloos <julien.coloos@gmail.com>
2018-06-04 21:30:17 +0200

    Add helper to get class location
    Useful to determine where an application is running

--- core/src/main/scala/suiryc/scala/misc/Util.scala
+++ core/src/main/scala/suiryc/scala/misc/Util.scala
@@ -1,5 +1,6 @@
 package suiryc.scala.misc
 
+import java.nio.file.{Files, Path, Paths}
 import scala.reflect.ClassTag
 
 /** Misc utilities. */
@@ -14,5 +15,18 @@ object Util {
    */
   def wrapNull[T: ClassTag](a: Array[T]): Array[T] =
     Option(a).getOrElse(new Array[T](0))
+
+  /**
+   * Gets a class location.
+   * Gets path for given class: parent folder of its jar, or the running folder.
+   */
+  def classLocation[T: ClassTag]: Path = {
+    val ct = implicitly[ClassTag[T]]
+    // See: http://stackoverflow.com/a/12733172
+    val appPath = Paths.get(ct.runtimeClass.getProtectionDomain.getCodeSource.getLocation.toURI)
+    // We either got the class jar (file), or the running folder
+    if (Files.isDirectory(appPath)) appPath
+    else appPath.getParent
+  }
 
 }
