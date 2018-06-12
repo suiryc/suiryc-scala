69e52135d080688ae4972fa9012062bd5dfc2e52
Julien Coloos <julien.coloos@gmail.com>
2018-06-12 19:42:16 +0200

    Clean the configuration before saving it
    Only write changed (non-default) values

--- core/src/main/scala/suiryc/scala/settings/PortableSettings.scala
+++ core/src/main/scala/suiryc/scala/settings/PortableSettings.scala
@@ -1,9 +1,10 @@
 package suiryc.scala.settings
 
-import com.typesafe.config.{Config, ConfigFactory, ConfigRenderOptions, ConfigValue}
+import com.typesafe.config._
 import com.typesafe.scalalogging.StrictLogging
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path}
+import scala.collection.JavaConverters._
 
 /**
  * Portable settings.
@@ -51,22 +52,91 @@ class PortableSettings(filepath: Path, var _config: Config, ref: Config) {
 
   /** Saves the config. */
   def save(): Unit = this.synchronized {
-    save(filepath)
+    save(filepath, backup = false)
   }
 
   /** Backups the config. */
   protected def backup(): Unit = this.synchronized {
     if (!backupDone) {
-      save(PortableSettings.backupPath(filepath))
+      save(PortableSettings.backupPath(filepath), backup = true)
       backupDone = true
     }
   }
 
   /** Actual config saving (given file path). */
-  protected def save(path: Path): Unit = {
-    val renderOptions = ConfigRenderOptions.defaults.setOriginComments(false).setJson(false)
-    Files.write(path, config.root.render(renderOptions).getBytes(StandardCharsets.UTF_8))
+  protected def save(path: Path, backup: Boolean): Unit = {
+    clean(config) match {
+      case Some(cleanedConfig) =>
+        val renderOptions = ConfigRenderOptions.defaults.setOriginComments(false).setJson(false)
+        Files.write(path, cleanedConfig.root.render(renderOptions).getBytes(StandardCharsets.UTF_8))
+
+      case None =>
+        // We could delete the configuration as there is no change from the
+        // reference. But it's more user-friendly to simply say so in the file.
+        // We only delete the backup file in this case.
+        if (backup && Files.exists(path)) Files.delete(path)
+        if (!backup) Files.write(path, "# No value overrides the reference configuration\n".getBytes(StandardCharsets.UTF_8))
+    }
     ()
+  }
+
+  @scala.annotation.tailrec
+  private def isEmpty(objs: List[ConfigObject]): Boolean = {
+    // If there is no more entry to check, we are empty
+    if (objs.isEmpty) true
+    else {
+      val head = objs.head
+      val tail = objs.tail
+      // If next object is empty, check the rest
+      if (head.isEmpty) isEmpty(objs.tail)
+      else {
+        val values = head.values
+        val children = values.asScala.toList.takeWhile(_.valueType == ConfigValueType.OBJECT)
+        // If there are non objects, we are not empty; otherwise check all
+        // children are empty.
+        if (values.size != children.size) false
+        else isEmpty(tail ::: children.map(_.asInstanceOf[ConfigObject]))
+      }
+    }
+  }
+
+  private case class ObjectEntry(path: String, value: ConfigObject)
+
+  private def clean(config: Config): Option[Config] = {
+    @scala.annotation.tailrec
+    def clean(config: Config, entries: List[ObjectEntry]): Option[Config] = {
+      // If there is no more entry to check, we are done
+      if (entries.isEmpty) Some(config)
+      else {
+        val head = entries.head
+        val tail = entries.tail
+        // If next entry is empty, remove its path and check the rest
+        if (isEmpty(List(head.value))) {
+          // If we are to remove the root path, whole config is empty
+          if (head.path.isEmpty) None
+          else clean(config.withoutPath(head.path), tail)
+        } else {
+          // Adds children objects for next.
+          val newEntries = head.value.entrySet.asScala.toList.filter(_.getValue.valueType == ConfigValueType.OBJECT).map { entry =>
+            ObjectEntry(
+              path = List(head.path, entry.getKey).filter(_.length > 0).mkString("."),
+              value = entry.getValue.asInstanceOf[ConfigObject]
+            )
+          }
+          clean(config, tail ::: newEntries)
+        }
+      }
+    }
+
+    // We first remove unchanged values
+    val cleaned1 = config.entrySet.asScala.foldLeft(config) { (acc, entry) =>
+      val key = entry.getKey
+      if (ref.hasPath(key) && ref.getValue(key) == entry.getValue) acc.withoutPath(key)
+      else acc
+    }
+
+    // Then we can remove empty objects
+    clean(cleaned1, List(ObjectEntry("", cleaned1.root)))
   }
 
 }
