79094cd8a8f4f5faa1729b53c3504e15d5f2a1b4
Julien Coloos <julien.coloos@gmail.com>
2018-06-14 18:21:19 +0200

    Fix PathsEx helpers
    Rely on Paths and Path functions to properly handle cases: actual
    separator, string path ending with separator, file or folder without
    extension.
    Also explicitly return empty extension if filename has none.

--- core/src/main/scala/suiryc/scala/io/PathsEx.scala
+++ core/src/main/scala/suiryc/scala/io/PathsEx.scala
@@ -2,10 +2,11 @@ package suiryc.scala.io
 
 import java.nio.file.{Path, Paths}
 
-
+/** Path (and filename) helpers. */
 object PathsEx {
 
-  def get(path: String): Path =
+  /** Gets path (resolves leading '~' as user home). */
+  def get(path: String): Path = {
     if (path.startsWith("~")) {
       val rest = path.substring(2)
       val home = RichFile.userHome.toPath
@@ -13,22 +14,31 @@ object PathsEx {
       else home.resolve(rest)
     }
     else Paths.get(path)
+  }
 
+  /** Gets filename (or basename; hierarchy leaf). */
   def filename(name: String): String =
-    name.split("/").toList.reverse.head
+    Paths.get(name).getFileName.toString
 
-  def atomicName(name: String): String =
-    /* keep filename */
-    filename(name).
-      /* without extension */
-      split("""\.""").reverse.tail.reverse.mkString(".")
+  /** Gets 'atomic' name (filename without extension). */
+  def atomicName(name: String): String = {
+    val parts = filename(name).split('.')
+    if (parts.length > 1) parts.view(0, parts.length - 1).mkString(".")
+    else parts.head
+  }
 
+  /** Gets 'atomic' name (filename without extension). */
   def atomicName(path: Path): String =
     atomicName(path.getFileName.toString)
 
-  def extension(name: String): String =
-    name.split("""\.""").toList.reverse.head
+  /** Gets extension (empty if none). */
+  def extension(name: String): String = {
+    val parts = filename(name).split('.')
+    if (parts.length > 1) parts(parts.length - 1)
+    else ""
+  }
 
+  /** Gets extension. */
   def extension(path: Path): String =
     extension(path.getFileName.toString)
 
