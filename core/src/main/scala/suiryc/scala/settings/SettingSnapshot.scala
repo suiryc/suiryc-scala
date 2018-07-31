05106f416ef9d2b683f6cbd414688f986a6c8619
Julien Coloos <julien.coloos@gmail.com>
2018-07-31 13:01:42 +0200

    Associate draft value to setting snapshot
    Instead of having caller explicitly change values when needed and
    possibly check whether there was any change ('changed') afterwards,
    use an intermediate property field holding any change, and let caller
    apply all changes (if any) when applicable.
    This simplifies the caller code, which only needs one call to apply
    changes (and know in return if there was any).
    
    Also have helpers to explicitly refresh draft value (from external input
    like UI), test for changes against original/default value, and apply
    side-effect upon actual value change.

--- core/src/main/scala/suiryc/scala/settings/SettingSnapshot.scala
+++ core/src/main/scala/suiryc/scala/settings/SettingSnapshot.scala
@@ -1,7 +1,6 @@
 package suiryc.scala.settings
 
-import com.typesafe.config.ConfigValue
-import javafx.beans.property.Property
+import javafx.beans.property.{Property, SimpleObjectProperty}
 
 /**
  * Setting snapshot.
@@ -10,23 +9,129 @@ import javafx.beans.property.Property
  */
 trait SettingSnapshot[T] {
 
-  /** Original value. */
-  protected val originalValue: T =
-    getValue
+  // Notes:
+  // We wish to
+  //  - allow changing values (e.g. through an UI) without applying them until
+  //    needed/validated
+  //  - know whether there is a change to apply
+  //  - be able to reset value to a default when applicable
+  //  - have an optional side-effect if we apply a value change
+  //
+  // To do so, along the underlying setting, we
+  //  - keep any possible change in a 'draft' value
+  //  - remember a possible default value (initial value by default)
+  //  - have code to change draft value (from external input, e.g. UI)
+  //  - have code to call on change (side-effect)
+  // The draft is kept as a Property so that caller may listen to any change
+  // (e.g. to trigger side-effect when resetting to default value).
+  //
+  // Special note:
+  // Legacy code was changing underlying setting value externally, use 'changed'
+  // to determine whether any change was actually done, and when applicable
+  // would call 'reset' to revert changes.
+  // Going through an intermediate 'draft' value is safer, and associating code
+  // to refresh the value makes the caller job easier.
 
-  /** Gets current value. */
+  /** Original value. */
+  protected val originalValue: T = getValue
+
+  /** Default value (original one by default). */
+  protected var defaultValue: T = originalValue
+
+  /** Code to execute upon actual value change (side-effect). */
+  protected var onChange: Option[T ⇒ Unit] = None
+
+  /** Code to execute to refresh draft value (from external input). */
+  protected var onRefreshDraft: Option[() ⇒ T] = None
+
+  /** Draft value (prepared to apply value change). */
+  val draft = new SimpleObjectProperty[T](originalValue)
+
+  /** Gets underlying setting value. */
   protected def getValue: T
 
-  /** Sets value. */
+  /** Sets underlying setting value. */
   protected def setValue(v: T): Unit
 
-  /** Gets whether the setting was changed. */
-  def changed(): Boolean =
-    originalValue != getValue
+  /**
+   * Changes underlying setting value.
+   *
+   * Sets value and executes associated code if any.
+   */
+  protected def applyChange(v: T): Unit = {
+    setValue(v)
+    onChange.foreach(_(v))
+  }
+
+  /** Sets default value. */
+  def withDefault(v: T): SettingSnapshot[T] = {
+    defaultValue = v
+    this
+  }
+
+  /** Sets code to execute upon actual value change. */
+  def setOnChange(f: T ⇒ Unit): SettingSnapshot[T] = {
+    onChange = Some(f)
+    this
+  }
+
+  /** Sets code to execute to refresh draft value (form external input). */
+  def setOnRefreshDraft(f: ⇒ T): SettingSnapshot[T] = {
+    onRefreshDraft = Some(() ⇒ f)
+    this
+  }
+
+  /** Gets whether the underlying setting value was changed. */
+  def changed(): Boolean = originalValue != getValue
 
   /** Resets the setting to its initial value. */
-  def reset(): Unit = {
-    if (changed()) setValue(originalValue)
+  def reset(): Unit = if (changed()) applyChange(originalValue)
+
+  /**
+   * Gets draft/refresh value.
+   *
+   * Gets current draft value.
+   * If requested, gets the refreshed value (without actually refreshing the
+   * draft value).
+   */
+  def getDraftValue(refreshed: Boolean = true): T = {
+    if (refreshed) onRefreshDraft.map(_()).getOrElse(draft.get)
+    else draft.get
+  }
+
+  /**
+   * Whether draft value was changed.
+   *
+   * Compares current/refreshed draft value to original or default one.
+   */
+  def isDraftChanged(original: Boolean = true, refreshed: Boolean = true): Boolean = {
+    val draftValue = getDraftValue(refreshed)
+    if (original) draftValue != originalValue
+    else draftValue != defaultValue
+  }
+
+  /** Refreshes, applies draft value and returns whether it was changed. */
+  def applyDraft(): Boolean = {
+    refreshDraft()
+    if (isDraftChanged(refreshed = false)) {
+      applyChange(draft.get)
+      true
+    } else false
+  }
+
+  /** Refreshes draft value. */
+  def refreshDraft(): Unit = onRefreshDraft.foreach(f ⇒ draft.set(f()))
+
+  /**
+   * Sets draft to original or default value.
+   *
+   * If asked, first refresh the draft value (useful to trigger value change
+   * if the draft value was not up-to-date yet).
+   */
+  def resetDraft(original: Boolean = true, refresh: Boolean = true): Unit = {
+    if (refresh) refreshDraft()
+    if (original) draft.set(originalValue)
+    else draft.set(defaultValue)
   }
 
 }
@@ -91,20 +196,33 @@ class SettingsSnapshot {
   private var snapshots: List[SettingSnapshot[_]] = Nil
 
   /** Adds setting snapshots. */
-  def add(others: Seq[SettingSnapshot[_]]): Unit =
-    snapshots ++= others.toList
+  def add(others: Seq[SettingSnapshot[_]]): Unit = snapshots ++= others.toList
 
   /** Adds setting snapshots. (vararg variant) */
-  def add(others: SettingSnapshot[_]*)(implicit d: DummyImplicit): Unit =
-    add(others)
+  def add(others: SettingSnapshot[_]*)(implicit d: DummyImplicit): Unit = add(others)
 
   /** Gets whether any setting changed. */
-  def changed(): Boolean =
-    snapshots.exists(_.changed())
+  def changed(): Boolean = snapshots.exists(_.changed())
 
   /** Resets all settings. */
-  def reset(): Unit = {
-    snapshots.foreach(_.reset())
-  }
+  def reset(): Unit = snapshots.foreach(_.reset())
+
+  /**
+   * Whether any draft value was changed.
+   *
+   * Compares current/refreshed draft value to original or default one.
+   */
+  def isDraftChanged(original: Boolean = true, refreshed: Boolean = true): Boolean =
+    snapshots.exists(_.isDraftChanged(original, refreshed))
+
+  /** Refreshes, applies drafts values and returns whether any was changed. */
+  def applyDraft(): Boolean = snapshots.foldLeft(false)(_ || _.applyDraft())
+
+  /** Refreshes drafts values. */
+  def refreshDraft(): Unit = snapshots.foreach(_.refreshDraft())
+
+  /** Sets drafts to original or default value. */
+  def resetDraft(original: Boolean = true, refresh: Boolean = true): Unit =
+    snapshots.foreach(_.resetDraft(original, refresh))
 
 }
