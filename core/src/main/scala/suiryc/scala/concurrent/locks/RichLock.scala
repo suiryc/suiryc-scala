cda73a3c46bd092d6a3485e50528b6831d20aa03
Julien Coloos <julien.coloos@gmail.com>
2018-08-22 23:18:54 +0200

    Add Lock helper

---
+++ core/src/main/scala/suiryc/scala/concurrent/locks/RichLock.scala
@@ -0,0 +1,30 @@
+package suiryc.scala.concurrent.locks
+
+import java.util.concurrent.locks.Lock
+
+/**
+ * Rich lock.
+ *
+ * Adds helper to automatically lock/unlock around some code.
+ */
+class RichLock(val underlying: Lock) extends AnyVal {
+
+  def withLock[A](f: ⇒ A): A = {
+    underlying.lock()
+    try {
+      f
+    } finally {
+      underlying.unlock()
+    }
+  }
+
+}
+
+object RichLock {
+
+  import scala.language.implicitConversions
+
+  implicit def toRichLock(lock: Lock): RichLock =
+    new RichLock(lock)
+
+}
