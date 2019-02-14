59b69764bcf5c3b635a005e0f593db328e55dd84
Julien Coloos <julien.coloos@gmail.com>
2019-02-14 17:52:39 +0100

    Cleanup Cancellable
    Use more sensible 'isCancelled' over 'cancelled' function name.
    Helpers to build one Cancellable wrapping multiple Cancellables.

--- core/src/main/scala/suiryc/scala/concurrent/Cancellable.scala
+++ core/src/main/scala/suiryc/scala/concurrent/Cancellable.scala
@@ -1,23 +1,58 @@
 package suiryc.scala.concurrent
 
-
+// Note:
+// We purposely have 'Cancelled' be a class instead of an object so that we have
+// distincts stacktraces when used.
 case class Cancelled() extends RuntimeException("Cancelled")
 
+/** Simple cancellable resource. */
 class Cancellable {
 
-  private var _cancelled = false
+  private var cancelled = false
 
-  def cancelled(): Boolean = _cancelled
+  /** Gets whether this Cancellable has been cancelled. */
+  def isCancelled: Boolean = cancelled
 
-  def cancel() {
-    _cancelled = true
+  /** Cancels. */
+  def cancel(): Unit = {
+    cancelled = true
   }
 
-  def check(cleanup: => Unit = {}) {
-    if (_cancelled) {
+  /**
+   * Checks whether we have been cancelled.
+   *
+   * If we were not cancelled, nothing is done.
+   * Otherwise the optional cleanup code is executed and Cancelled is thrown.
+   *
+   * @param cleanup code to execute if we had been cancelled
+   * @throws Cancelled if we had been cancelled
+   */
+  def check(cleanup: ⇒ Unit = {}): Unit = {
+    if (cancelled) {
       cleanup
       throw Cancelled()
     }
   }
+
+}
+
+object Cancellable {
+
+  /** Builds a Cancellable wrapping multiple Cancellables. */
+  def apply(cancellables: Seq[Cancellable]): Cancellable = {
+    new Cancellable {
+      override def cancel(): Unit = {
+        cancellables.foreach(_.cancel())
+        super.cancel()
+      }
+    }
+  }
+
+  /**
+   * Builds a Cancellable wrapping multiple Cancellables.
+   *
+   * vararg variant.
+   */
+  def apply(cancellables: Cancellable*)(implicit d: DummyImplicit): Cancellable = Cancellable(cancellables)
 
 }
