32777d30eee4daee2fc366be50da805cf680f4b6
Julien Coloos <julien.coloos@gmail.com>
2019-02-15 22:47:24 +0100

    Fix and cleanup LineSplitterOutputStream
    Fixes:
      - properly handle input data larger than decoding buffer
      - handle line-ending written separately from line content
      - properly write pending chars upon closing
    
    Add unit tests and comments.

--- core/src/main/scala/suiryc/scala/io/LineSplitterOutputStream.scala
+++ core/src/main/scala/suiryc/scala/io/LineSplitterOutputStream.scala
@@ -2,7 +2,8 @@ package suiryc.scala.io
 
 import java.io.OutputStream
 import java.nio.{ByteBuffer, CharBuffer}
-import java.nio.charset.{Charset, CoderResult}
+import java.nio.charset.{Charset, CharsetDecoder, CoderResult}
+import suiryc.scala.misc.Util
 
 /**
  * OutputStream that breaks data into lines.
@@ -18,82 +19,99 @@ class LineSplitterOutputStream(
 
   import LineSplitterOutputStream._
 
-  // Note: once flushed, the charset decoder *must not* be used again.
-  // Thus:
+  // Notes:
+  // Once flushed, the charset decoder *must not* be used again. Thus:
   //  - only flush it upon closing the stream
   //  - stream 'flush' does nothing, and needs not be overridden
   //  - drop anything written after closing the stream
+  //
+  // We have two buffers:
+  //  - 'charBuffer': fixed-length where decoded content is written to
+  //  - 'line': line being built from decoded content (charBuffer)
+  // When input is received, it is decoded ('charBuffer') and line endings are
+  // searched for to build lines one at a time ('line' written once ending has
+  // been reached). The whole input is processed, and trailing decoded content
+  // (i.e. no line ending) is simply stored in 'line' ('charBuffer' emptied).
+  // Upon closing, we then only need to flush the decoder and write the pending
+  // 'line' if non empty.
 
+  // Whether we are closed.
   protected var closed = false
-  protected val decoder = charset.newDecoder
-  protected val charBuffer = CharBuffer.allocate(defaultBufferSize)
+  // Characters decoder.
+  protected val decoder: CharsetDecoder = charset.newDecoder
+  // Decoded content buffer.
+  protected val charBuffer: CharBuffer = CharBuffer.allocate(defaultBufferSize)
+  // Decoded line content (no line ending reached yet).
   protected val line = new StringBuilder()
 
-  override def write(b: Int) {
-    if (!closed)
-      write(List[Byte](b.asInstanceOf[Byte]).toArray)
+  override def write(b: Int): Unit = {
+    if (!closed) write(Array[Byte](b.toByte))
   }
 
-  override def write(b: Array[Byte], off: Int, len: Int) {
-    if (!closed)
-      process(ByteBuffer.wrap(b, off, len), flush = false)
+  override def write(b: Array[Byte], off: Int, len: Int): Unit = {
+    if (!closed) process(ByteBuffer.wrap(b, off, len), flush = false)
   }
 
-  override def close() {
+  override def close(): Unit = {
     if (!closed) {
       process(ByteBuffer.wrap(new Array[Byte](0)), flush = true)
       closed = true
     }
   }
 
-  protected def process(bb: ByteBuffer, flush: Boolean) {
+  protected def process(bb: ByteBuffer, flush: Boolean): Unit = {
     @scala.annotation.tailrec
-    def loop(f: => CoderResult) {
-      val result = f
-
-      if (charBuffer.position() > 0)
-        process(flush)
+    def loop(f: () ⇒ CoderResult) {
+      val result = f()
+      // Process decoded content if any.
+      if (charBuffer.position() > 0) processBuffer()
+      // If charBuffer was too small to decode the whole input ('overflow'),
+      // keep on decoding (charBuffer content has been moved to 'line').
       if (result.isOverflow) loop(f)
     }
 
-    loop(decoder.decode(bb, charBuffer, flush))
-    if (flush)
-      loop(decoder.flush(charBuffer))
+    // Decode input buffer.
+    loop(() ⇒ decoder.decode(bb, charBuffer, flush))
+    if (flush) {
+      // Flush decoder and process decoded content if any.
+      loop(() ⇒ decoder.flush(charBuffer))
+      if (charBuffer.position() > 0) processBuffer()
+      // Write pending chars if any.
+      if (line.nonEmpty) writeLine()
+    }
   }
 
-  protected def process(flush: Boolean) {
+  protected def writeLine() {
+    if (line.endsWith("\r")) line.setLength(line.length - 1)
+    writer.write(line.toString())
+    line.clear()
+  }
 
+  protected def processBuffer(): Unit = {
     val array = charBuffer.array
-
-    def output() {
-      if (line.endsWith("\r"))
-        line.setLength(line.length - 1)
-      writer.write(line.toString())
-      line.clear()
-    }
+    val length = charBuffer.position()
 
     @scala.annotation.tailrec
-    def loop(offset: Int) {
-      if (offset < charBuffer.position()) {
-        val nextOffset = {
-          val v = array.indexOf('\n', offset)
-          if ((v >= 0) || !flush) v
-          else charBuffer.position()
-        }
-
+    def loop(offset: Int): Unit = {
+      if (offset < length) {
+        // Search for next LF (up to actual written buffer position)
+        val nextOffset = Util.indexOf(array, '\n', offset, length)
         if (nextOffset >= 0) {
+          // LF found: append to line, write it and keep on processing.
           line.appendAll(array, offset, nextOffset - offset)
-          output()
+          writeLine()
           loop(nextOffset + 1)
-        }
-        else {
-          line.appendAll(array, offset, charBuffer.position() - offset)
+        } else {
+          // LF not found: append remaining decoded content to line.
+          line.appendAll(array, offset, length - offset)
           ()
         }
       }
     }
 
+    // Process decoded content to find lines.
     loop(0)
+    // Empty decoded (and now processed) content.
     charBuffer.position(0)
     ()
   }
---
+++ core/src/test/scala/suiryc/scala/io/LineSplitterOutputStreamSpec.scala
@@ -0,0 +1,138 @@
+package suiryc.scala.io
+
+import java.io.PrintStream
+import java.nio.charset.StandardCharsets
+import org.scalatest.{Matchers, WordSpec}
+
+// scalastyle:off regex
+class LineSplitterOutputStreamSpec extends WordSpec with Matchers {
+
+  import LineSplitterOutputStreamSpec._
+
+  "LineSplitterOutputStream" should {
+
+    "handle writing bytes" in {
+      val setup = new TestSetup
+      setup.printer.write('A')
+      setup.printer.write('B')
+      setup.printer.write('C')
+      setup.writer.getLines shouldBe empty
+      setup.printer.write('\n')
+      setup.writer.getLines shouldBe List("ABC")
+      setup.printer.close()
+      setup.writer.getLines shouldBe empty
+    }
+
+    "handle writing byte arrays" in {
+      val setup = new TestSetup
+      setup.printer.write("ABC\n".getBytes(UTF8))
+      setup.writer.getLines shouldBe List("ABC")
+      setup.printer.close()
+      setup.writer.getLines shouldBe empty
+    }
+
+    "handle writing partial byte arrays" in {
+      val setup = new TestSetup
+      setup.printer.write("ZDEF\n".getBytes(UTF8), 1, 3)
+      setup.writer.getLines shouldBe empty
+      setup.printer.write("\nZ\n".getBytes(UTF8), 0, 1)
+      setup.writer.getLines shouldBe List("DEF")
+      setup.printer.close()
+      setup.writer.getLines shouldBe empty
+    }
+
+    "handle writing lines" in {
+      val setup = new TestSetup
+      setup.printer.println("ABC")
+      setup.writer.getLines shouldBe List("ABC")
+      setup.printer.println(1)
+      setup.printer.close()
+      setup.writer.getLines shouldBe List("1")
+    }
+
+    "handle multiple lines" in {
+      val setup = new TestSetup
+      setup.printer.println("ABC\nDEF")
+      setup.writer.getLines shouldBe List("ABC", "DEF")
+      setup.printer.write("abc\ndef\n".getBytes(UTF8))
+      setup.writer.getLines shouldBe List("abc", "def")
+    }
+
+    "handle empty lines" in {
+      val setup = new TestSetup
+      setup.printer.println("\nABC\n\nDEF\n")
+      setup.writer.getLines shouldBe List("", "ABC", "", "DEF", "")
+    }
+
+    "handle CR" in {
+      val setup = new TestSetup
+      setup.printer.write("ABC\r\n".getBytes(UTF8))
+      setup.writer.getLines shouldBe List("ABC")
+      setup.printer.write("ABC\r\r\n".getBytes(UTF8))
+      setup.writer.getLines shouldBe List("ABC\r")
+      setup.printer.close()
+      setup.writer.getLines shouldBe empty
+    }
+
+    "handle CR and LF written separately" in {
+      val setup = new TestSetup
+      setup.printer.write("ABC\r".getBytes(UTF8))
+      setup.writer.getLines shouldBe empty
+      setup.printer.write("\nDEF\r\n".getBytes(UTF8))
+      setup.writer.getLines shouldBe List("ABC", "DEF")
+    }
+
+    "do nothing on nominal flushing" in {
+      val setup = new TestSetup
+      setup.printer.write("ABC".getBytes(UTF8))
+      setup.writer.getLines shouldBe empty
+      setup.printer.flush()
+      setup.writer.getLines shouldBe empty
+    }
+
+    "handle closing without pending data" in {
+      val setup = new TestSetup
+      setup.printer.close()
+      setup.writer.getLines shouldBe empty
+    }
+
+    "handle closing with pending data" in {
+      val setup = new TestSetup
+      setup.printer.write("ABC".getBytes(UTF8))
+      setup.writer.getLines shouldBe empty
+      setup.printer.close()
+      setup.writer.getLines shouldBe List("ABC")
+    }
+
+  }
+
+}
+
+object LineSplitterOutputStreamSpec {
+
+  private val UTF8 = StandardCharsets.UTF_8
+
+  class TestSetup {
+    val writer = new MemoryLineWriter
+    val output = new LineSplitterOutputStream(writer, UTF8)
+    val printer = new PrintStream(output, false, UTF8)
+  }
+
+  class MemoryLineWriter extends LineWriter {
+
+    private var lines = List.empty[String]
+
+    override def write(line: String): Unit = {
+      lines :+= line
+    }
+
+    def getLines: List[String] = {
+      val r = lines
+      lines = Nil
+      r
+    }
+
+  }
+
+}
+// scalastyle:on regex
