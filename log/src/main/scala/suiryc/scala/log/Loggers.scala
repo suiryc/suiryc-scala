4841307317706c1f776b6edbb103c89ccf76424d
Julien Coloos <julien.coloos@gmail.com>
2019-05-07 17:38:52 +0200

    Helper to capture IO only if logger does not use console

--- log/src/main/scala/suiryc/scala/log/Loggers.scala
+++ log/src/main/scala/suiryc/scala/log/Loggers.scala
@@ -1,16 +1,47 @@
 package suiryc.scala.log
 
 import ch.qos.logback.classic.{Level, LoggerContext}
+import ch.qos.logback.core.ConsoleAppender
 import org.slf4j.LoggerFactory
+import suiryc.scala.io.SystemStreams
 
 
 object Loggers {
 
+  lazy private val rootLogger = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME).asInstanceOf[ch.qos.logback.classic.Logger]
+
   val loggerContext: LoggerContext = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+
+  /** Whether (root) logger uses console as output. */
+  lazy private val useConsole: Boolean = {
+    val it = rootLogger.iteratorForAppenders()
+    def loop(): Boolean = {
+      if (!it.hasNext) false
+      else if (it.next.isInstanceOf[ConsoleAppender[_]]) true
+      else loop()
+    }
+    loop()
+  }
+
+  /** Capture IO unless logger uses console. */
+  def captureIo(): Option[SystemStreams] = {
+    if (!useConsole) {
+      Some {
+        SystemStreams.replace(
+          SystemStreams.loggerOutput("stdout"),
+          SystemStreams.loggerOutput("stderr", error = true)
+        )
+      }
+    } else {
+      // scalastyle:off regex
+      System.out.println("Not capturing IO: logger uses console")
+      // scalastyle:on regex
+      None
+    }
+  }
 
   /** Sets root logger level. */
   def setLevel(level: Level) {
-    val rootLogger = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME).asInstanceOf[ch.qos.logback.classic.Logger]
     rootLogger.setLevel(level)
   }
 
