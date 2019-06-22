9b1b02e68f9e8c41f6d45eebf572380a6e0cee84
Julien Coloos <julien.coloos@gmail.com>
2019-06-22 22:40:40 +0200

    Helper class to cache a computed value

---
+++ core/src/main/scala/suiryc/scala/util/Cached.scala
@@ -0,0 +1,31 @@
+package suiryc.scala.util
+
+/** Computed value caching. */
+class Cached[A](compute: () ⇒ A) extends (() ⇒ A) {
+
+  // Cached value (if already computed)
+  private var cached = Option.empty[A]
+
+  /** Gets the value (and caches it). */
+  def value: A = this.synchronized {
+    cached.getOrElse {
+      val v = compute()
+      cached = Some(v)
+      v
+    }
+  }
+
+  override def apply(): A = value
+
+  /** Invalidates the cached value. */
+  def invalidate(): Unit = this.synchronized {
+    cached = None
+  }
+
+}
+
+object Cached {
+
+  def apply[A](compute: () ⇒ A): Cached[A] = new Cached(compute)
+
+}
