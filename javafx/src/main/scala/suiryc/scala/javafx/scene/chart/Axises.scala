7adbbcbcbd40a6945375a0f3a0b7f601c04e0df6
Julien Coloos <julien.coloos@gmail.com>
2019-07-11 23:52:26 +0200

    JavaFX Axis helper function to compute value at display position
    Sometimes we cannot rely on the JavaFX function to do this, and have to
    compute it ourself.

--- javafx/src/main/scala/suiryc/scala/javafx/scene/chart/Axises.scala
+++ javafx/src/main/scala/suiryc/scala/javafx/scene/chart/Axises.scala
@@ -6,7 +6,7 @@ import javafx.scene.chart.NumberAxis
 object Axises {
 
   /** Gets index display position relatively to axis. */
-  def getDisplayPosition(axis: NumberAxis, v: Number): Double = {
+  def getDisplayPosition(axis: NumberAxis, value: Number): Double = {
     // Notes:
     // When autoranging is on, axis 'getDisplayPosition' is usually right.
     // Otherwise it may be wrong, probably when lower bound is being changed
@@ -15,10 +15,22 @@ object Axises {
     // To workaround this, we compute the value with the real lower bound:
     //  offset + (value - lowerBound) * scale
     // 'offset' is 0 for horizontal axis, and the axis height for vertical one.
-    if (axis.isAutoRanging) axis.getDisplayPosition(v)
+    if (axis.isAutoRanging) axis.getDisplayPosition(value)
     else {
       val offset = if (axis.getSide.isVertical) axis.getHeight else 0
-      offset + (v.doubleValue - axis.getLowerBound) * axis.getScale
+      offset + (value.doubleValue - axis.getLowerBound) * axis.getScale
+    }
+  }
+
+  /** Gets index displayed at position (relatively to axis). */
+  def getValueForDisplay(axis: NumberAxis, displayPosition: Double): Number = {
+    // We workaround the same issue as in getDisplayPosition. The formula is:
+    //  ((displayPosition - offset) / axis.getScale) + axis.getLowerBound
+    // (converted to real axis value)
+    if (axis.isAutoRanging) axis.getValueForDisplay(displayPosition)
+    else {
+      val offset = if (axis.getSide.isVertical) axis.getHeight else 0
+      axis.toRealValue(((displayPosition - offset) / axis.getScale) + axis.getLowerBound)
     }
   }
 
