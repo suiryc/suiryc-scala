292ffaedb352f13a0cf80b8eff177d6ccec1a6f8
Julien Coloos <julien.coloos@gmail.com>
2019-11-02 16:25:55 +0100

    Helper to set default application configuration
    By default point to an embedded application-override.conf that contains
    3rd-party default configuration overridings and include "application"
    to fallback to the usual application.conf.
    This is useful for libraries/frameworks that do load their configuration
    but use application.conf instead of reference.conf.
    
    Allow specifying a given file path or resource name to use as default
    application configuration.
    
    Also have a dedicated loader that only resolves configuration after all
    fallback layers have been set (the standard configuration loading does
    resolve the reference configuration before loading the application one).

--- core/src/main/scala/suiryc/scala/Configuration.scala
+++ core/src/main/scala/suiryc/scala/Configuration.scala
@@ -1,26 +1,81 @@
 package suiryc.scala
 
-import com.typesafe.config.{Config, ConfigFactory, ConfigParseOptions}
+import com.typesafe.config.{Config, ConfigFactory}
+import java.nio.file.Path
 
 
 object Configuration {
 
-  /** Application configuration, with our reference.conf overriding. */
-  val loaded: Config = {
-    // Load configuration and have our own reference.conf overriding inserted at
-    // the right place. See https://github.com/lightbend/config/issues/342
-    // See how it is handled in Play:
-    // https://github.com/playframework/playframework/blob/master/framework/src/play/src/main/scala/play/api/Configuration.scala
-    // Note: parseResources do not resolve values, which is what we want here -
-    // at least in our reference.conf we point to a value that is present in
-    // our reference-overrides.conf.
+  private def getDefaultResourceName(resourceName: Option[String]): String = {
+    resourceName.getOrElse("application-override.conf")
+  }
+
+  /**
+   * Sets default application configuration.
+   *
+   * Useful for libraries/frameworks that do use ConfigFactory.load() without
+   * possibility to pass a specific Config instance.
+   * Must be called first, before any library/framework tries to load its
+   * configuration.
+   */
+  def setDefaultApplication(confPath: Option[Path] = None, resourceName: Option[String] = None): Unit = {
+    // Change the default application configuration unless already done. This
+    // is needed for libraries that use ConfigFactory.load() because some of
+    // them use 'application.conf' instead of 'reference.conf' and we usually
+    // have no guarantee that an application.conf embedded in our resources
+    // would actually override 3rd-party ones.
+    // We thus use an application-override.conf which must be included from
+    // the actual application configuration file, or can at least be used as
+    // default application configuration resource.
+    if ((System.getProperty("config.file") == null) &&
+      (System.getProperty("config.resource") == null) &&
+      (System.getProperty("config.url") == null)) {
+      confPath match {
+        case Some(p) => System.setProperty("config.file", p.toString)
+        case None    => System.setProperty("config.resource", getDefaultResourceName(resourceName))
+      }
+      ConfigFactory.invalidateCaches()
+    }
+  }
+
+  /**
+   * Loads a specific application configuration, with our overridings.
+   *
+   * Sets the default application configuration before loading it.
+   * Better called first, before any library/framework tries to load its
+   * configuration.
+   */
+  def load(confPath: Option[Path] = None, resourceName: Option[String] = None): Config = {
+    // Use requested configuration file and fallback to usual application and
+    // reference.conf.
+    // Either we load the file from an explicit path, or from the classpath.
+    setDefaultApplication(confPath, resourceName)
+    val appConf = confPath.map { path =>
+      ConfigFactory.parseFile(path.toFile)
+    }.getOrElse {
+      ConfigFactory.parseResources(getDefaultResourceName(resourceName))
+    }
+    // Build the configuration layers ourselves mainly because we wish for our
+    // configuration file (and application.conf) to truly override values in
+    // reference.conf, including variables substitutions.
+    // By default (e.g. ConfigFactory.load()), reference.conf is resolved first
+    // then overridden with application.conf (which is then resolved too). For
+    // both, environment variables can override values.
+    // So if reference.conf contains 'key-b = ${key-a}', overriding 'key-a'
+    // value only affects 'key-b' when done through environment variables, not
+    // through application.conf.
+    // Here we layer configuration files and only resolve the final result.
+    // See https://github.com/lightbend/config/issues/342
+    // In Play: https://github.com/playframework/playframework/blob/master/core/play/src/main/scala/play/api/Configuration.scala
     List(
       ConfigFactory.defaultOverrides(),
-      ConfigFactory.defaultApplication(ConfigParseOptions.defaults.setAllowMissing(true)),
-      ConfigFactory.parseResources("suiryc-scala/reference-overrides.conf"),
-      ConfigFactory.parseResources("reference.conf"),
-      ConfigFactory.defaultReference()
+      appConf,
+      ConfigFactory.defaultApplication(),
+      ConfigFactory.parseResources("reference.conf")
     ).reduceLeft(_.withFallback(_)).resolve()
   }
+
+  /** Default application configuration, with overridings. */
+  val loaded: Config = load()
 
 }
