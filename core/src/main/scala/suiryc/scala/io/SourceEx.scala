348e413d2fab7a8965ca89422091ffed8a36aa9b
Julien Coloos <julien.coloos@gmail.com>
2020-05-08 18:43:38 +0200

    Helper to (Source) get file lines iterator which auto-close upon EOF
    Also handle gzip compressed file on-the-fly.

--- core/src/main/scala/suiryc/scala/io/SourceEx.scala
+++ core/src/main/scala/suiryc/scala/io/SourceEx.scala
@@ -1,6 +1,8 @@
 package suiryc.scala.io
 
-import java.io.File
+import java.io.{BufferedInputStream, File, FileInputStream}
+import java.nio.file.Path
+import java.util.zip.GZIPInputStream
 import scala.io.{Codec, Source}
 
 
@@ -18,5 +20,39 @@ object SourceEx {
 
   def autoCloseFile[T](file: File, enc: String)(todo: Source => T): T =
     autoCloseFile(file)(todo)(Codec(enc))
+
+  @inline
+  private def getLinesIterator(path: Path)(implicit codec: Codec): Iterator[String] = {
+    new Iterator[String] {
+      private val source = if (path.getFileName.toString.endsWith(".gz")) {
+        Source.fromInputStream(new BufferedInputStream(new GZIPInputStream(new FileInputStream(path.toFile))))
+      } else {
+        Source.fromFile(path.toFile)
+      }
+      private val it = source.getLines()
+      override def hasNext: Boolean = {
+        val r = it.hasNext
+        if (!r) source.close()
+        r
+      }
+      override def next(): String = it.next
+    }
+  }
+
+  /**
+   * Gets lines iterator for given file path.
+   *
+   * Automatically handles (on-the-fly) uncompressing gzip files.
+   * Automatically closes source when EOF is reached.
+   * Note: usual iterator reading does call 'hasNext' on which we rely to
+   * close source when done.
+   */
+  def getLines(path: Path)(implicit codec: Codec): Iterator[String] = {
+    getLinesIterator(path)(codec)
+  }
+
+  def getLines(path: Seq[Path])(implicit codec: Codec): Iterator[String] = {
+    path.foldLeft(Iterator.empty[String])(_ ++ getLines(_))
+  }
 
 }
